aa11091958743434a49ae4c96e24dcbf77500709
Frédéric Delorme <frederic.delorme@web-context.com>
2014-04-14 17:23:20 +0200

    Add Service with a User entity, and a unitTest implementation start for this service. Add also Some Junit Eclipse wizard captures.

---
+++ samples/zelibrairie/src/test/java/com/webcontext/test/unit/libs/zelibraririe/UserServiceTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.webcontext.test.unit.libs.zelibraririe;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit Test for class UserService.
+ * 
+ * @author Frédéric Delorme<frederic.delorme@web-context.com>
+ *
+ */
+public class UserServiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
